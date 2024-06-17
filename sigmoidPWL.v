e7df5d06695cb7de15938f14648cb458831c58d6
EasoncoderTW <easonyehmobile1@gmail.com>
2024-06-18 02:40:43 +0800

    fix sign bit problem

--- sigmoidPWL.v
+++ sigmoidPWL.v
@@ -22,42 +22,42 @@ always @(posedge clk) begin
 		zero_stage_reg <= zero;
 	end
 end
-assign y = (zero_stage_reg)? 0: (x_stage_reg >> slope_stage_reg) + bias_stage_reg;
+assign y = (zero_stage_reg)? 0: ({16{x_stage_reg[15]},x_stage_reg} >> slope_stage_reg)[15:0] + bias_stage_reg;
 /**************** Compare and LUT *****************/
 always @(*) begin
-	if(x < 16'hf000) begin
+	if((x - 16'hf000)[15]) begin
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if(x < 16'hf7c0) begin
+	end else 	if((x - 16'hf7c0)[15]) begin
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if(x < 16'hfa18) begin
+	end else 	if((x - 16'hfa18)[15]) begin
 		slope = 16'h5;
 		zero = 0;
 		x_delta = 16'hf7c0;
-	end else 	if(x < 16'hfbb8) begin
+	end else 	if((x - 16'hfbb8)[15]) begin
 		slope = 16'h4;
 		zero = 0;
 		x_delta = 16'hfa18;
-	end else 	if(x < 16'hfdd0) begin
+	end else 	if((x - 16'hfdd0)[15]) begin
 		slope = 16'h3;
 		zero = 0;
 		x_delta = 16'hfbb8;
-	end else 	if(x < 16'h230) begin
+	end else 	if((x - 16'h230)[15]) begin
 		slope = 16'h2;
 		zero = 0;
 		x_delta = 16'hfdd0;
-	end else 	if(x < 16'h448) begin
+	end else 	if((x - 16'h448)[15]) begin
 		slope = 16'h3;
 		zero = 0;
 		x_delta = 16'h230;
-	end else 	if(x < 16'h5e8) begin
+	end else 	if((x - 16'h5e8)[15]) begin
 		slope = 16'h4;
 		zero = 0;
 		x_delta = 16'h448;
-	end else 	if(x < 16'h840) begin
+	end else 	if((x - 16'h840)[15]) begin
 		slope = 16'h5;
 		zero = 0;
 		x_delta = 16'h5e8;
@@ -67,33 +67,33 @@ always @(*) begin
 		x_delta = 16'h840;
 	end
 
-	if(x < 16'hf6d0) begin
+	if((x - 16'hf6d0)[15]) begin
 		bias = 16'h0;
-	end else 	if(x < 16'hfa18) begin
+	end else 	if((x - 16'hfa18)[15]) begin
 		bias = 16'h8;
-	end else 	if(x < 16'hfbb8) begin
+	end else 	if((x - 16'hfbb8)[15]) begin
 		bias = 16'h1c;
-	end else 	if(x < 16'hfc08) begin
+	end else 	if((x - 16'hfc08)[15]) begin
 		bias = 16'h39;
-	end else 	if(x < 16'hfd20) begin
+	end else 	if((x - 16'hfd20)[15]) begin
 		bias = 16'h30;
-	end else 	if(x < 16'hfdd0) begin
+	end else 	if((x - 16'hfdd0)[15]) begin
 		bias = 16'h38;
-	end else 	if(x < 16'hfdf0) begin
+	end else 	if((x - 16'hfdf0)[15]) begin
 		bias = 16'h84;
-	end else 	if(x < 16'hff20) begin
+	end else 	if((x - 16'hff20)[15]) begin
 		bias = 16'h7a;
-	end else 	if(x < 16'h1e8) begin
+	end else 	if((x - 16'h1e8)[15]) begin
 		bias = 16'h71;
-	end else 	if(x < 16'h230) begin
+	end else 	if((x - 16'h230)[15]) begin
 		bias = 16'h67;
-	end else 	if(x < 16'h2f0) begin
+	end else 	if((x - 16'h2f0)[15]) begin
 		bias = 16'h183;
-	end else 	if(x < 16'h448) begin
+	end else 	if((x - 16'h448)[15]) begin
 		bias = 16'h18b;
-	end else 	if(x < 16'h5e8) begin
+	end else 	if((x - 16'h5e8)[15]) begin
 		bias = 16'h1cd;
-	end else 	if(x < 16'h840) begin
+	end else 	if((x - 16'h840)[15]) begin
 		bias = 16'h1ea;
 	end else begin
 		bias = 16'h1fb;
--- tanhPWL.v
+++ tanhPWL.v
@@ -22,50 +22,50 @@ always @(posedge clk) begin
 		zero_stage_reg <= zero;
 	end
 end
-assign y = (zero_stage_reg)? 0: (x_stage_reg >> slope_stage_reg) + bias_stage_reg;
+assign y = (zero_stage_reg)? 0: ({16{x_stage_reg[15]},x_stage_reg} >> slope_stage_reg)[15:0] + bias_stage_reg;
 /**************** Compare and LUT *****************/
 always @(*) begin
-	if(x < 16'hf000) begin
+	if((x - 16'hf000)[15]) begin
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if(x < 16'hfb28) begin
+	end else 	if((x - 16'hfb28)[15]) begin
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if(x < 16'hfc48) begin
+	end else 	if((x - 16'hfc48)[15]) begin
 		slope = 16'h4;
 		zero = 0;
 		x_delta = 16'hfb28;
-	end else 	if(x < 16'hfd08) begin
+	end else 	if((x - 16'hfd08)[15]) begin
 		slope = 16'h3;
 		zero = 0;
 		x_delta = 16'hfc48;
-	end else 	if(x < 16'hfdd8) begin
+	end else 	if((x - 16'hfdd8)[15]) begin
 		slope = 16'h2;
 		zero = 0;
 		x_delta = 16'hfd08;
-	end else 	if(x < 16'hfee8) begin
+	end else 	if((x - 16'hfee8)[15]) begin
 		slope = 16'h1;
 		zero = 0;
 		x_delta = 16'hfdd8;
-	end else 	if(x < 16'h118) begin
+	end else 	if((x - 16'h118)[15]) begin
 		slope = 16'h0;
 		zero = 0;
 		x_delta = 16'hfee8;
-	end else 	if(x < 16'h228) begin
+	end else 	if((x - 16'h228)[15]) begin
 		slope = 16'h1;
 		zero = 0;
 		x_delta = 16'h118;
-	end else 	if(x < 16'h2f8) begin
+	end else 	if((x - 16'h2f8)[15]) begin
 		slope = 16'h2;
 		zero = 0;
 		x_delta = 16'h228;
-	end else 	if(x < 16'h3b8) begin
+	end else 	if((x - 16'h3b8)[15]) begin
 		slope = 16'h3;
 		zero = 0;
 		x_delta = 16'h2f8;
-	end else 	if(x < 16'h4d8) begin
+	end else 	if((x - 16'h4d8)[15]) begin
 		slope = 16'h4;
 		zero = 0;
 		x_delta = 16'h3b8;
@@ -75,57 +75,57 @@ always @(*) begin
 		x_delta = 16'h4d8;
 	end
 
-	if(x < 16'hf000) begin
+	if((x - 16'hf000)[15]) begin
 		bias = 16'h0;
-	end else 	if(x < 16'hf9d8) begin
+	end else 	if((x - 16'hf9d8)[15]) begin
 		bias = 16'hfdfd;
-	end else 	if(x < 16'hfc48) begin
+	end else 	if((x - 16'hfc48)[15]) begin
 		bias = 16'hfe06;
-	end else 	if(x < 16'hfc98) begin
+	end else 	if((x - 16'hfc98)[15]) begin
 		bias = 16'hfe1c;
-	end else 	if(x < 16'hfcf8) begin
+	end else 	if((x - 16'hfcf8)[15]) begin
 		bias = 16'hfe14;
-	end else 	if(x < 16'hfd08) begin
+	end else 	if((x - 16'hfd08)[15]) begin
 		bias = 16'hfe1d;
-	end else 	if(x < 16'hfd20) begin
+	end else 	if((x - 16'hfd20)[15]) begin
 		bias = 16'hfe36;
-	end else 	if(x < 16'hfdc0) begin
+	end else 	if((x - 16'hfdc0)[15]) begin
 		bias = 16'hfe2e;
-	end else 	if(x < 16'hfdd8) begin
+	end else 	if((x - 16'hfdd8)[15]) begin
 		bias = 16'hfe38;
-	end else 	if(x < 16'hfde8) begin
+	end else 	if((x - 16'hfde8)[15]) begin
 		bias = 16'hfe6e;
-	end else 	if(x < 16'hfea0) begin
+	end else 	if((x - 16'hfea0)[15]) begin
 		bias = 16'hfe65;
-	end else 	if(x < 16'hfed8) begin
+	end else 	if((x - 16'hfed8)[15]) begin
 		bias = 16'hfe6f;
-	end else 	if(x < 16'hfee8) begin
+	end else 	if((x - 16'hfee8)[15]) begin
 		bias = 16'hfe79;
-	end else 	if(x < 16'hfef0) begin
+	end else 	if((x - 16'hfef0)[15]) begin
 		bias = 16'hff05;
-	end else 	if(x < 16'hff18) begin
+	end else 	if((x - 16'hff18)[15]) begin
 		bias = 16'hfefc;
-	end else 	if(x < 16'hff50) begin
+	end else 	if((x - 16'hff50)[15]) begin
 		bias = 16'hfef4;
-	end else 	if(x < 16'h68) begin
+	end else 	if((x - 16'h68)[15]) begin
 		bias = 16'hfeec;
-	end else 	if(x < 16'hc8) begin
+	end else 	if((x - 16'hc8)[15]) begin
 		bias = 16'hfee4;
-	end else 	if(x < 16'h100) begin
+	end else 	if((x - 16'h100)[15]) begin
 		bias = 16'hfedb;
-	end else 	if(x < 16'h118) begin
+	end else 	if((x - 16'h118)[15]) begin
 		bias = 16'hfed2;
-	end else 	if(x < 16'h140) begin
+	end else 	if((x - 16'h140)[15]) begin
 		bias = 16'h102;
-	end else 	if(x < 16'h178) begin
+	end else 	if((x - 16'h178)[15]) begin
 		bias = 16'h10b;
-	end else 	if(x < 16'h228) begin
+	end else 	if((x - 16'h228)[15]) begin
 		bias = 16'h113;
-	end else 	if(x < 16'h2f8) begin
+	end else 	if((x - 16'h2f8)[15]) begin
 		bias = 16'h199;
-	end else 	if(x < 16'h3b8) begin
+	end else 	if((x - 16'h3b8)[15]) begin
 		bias = 16'h1d1;
-	end else 	if(x < 16'h4d8) begin
+	end else 	if((x - 16'h4d8)[15]) begin
 		bias = 16'h1eb;
 	end else begin
 		bias = 16'h1fb;
