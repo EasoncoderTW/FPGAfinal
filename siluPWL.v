a306430cead14ca58b9e3ce2f95109018463dfab
EasoncoderTW <easonyehmobile1@gmail.com>
2024-06-18 16:44:03 +0800

    merge conflit

--- siluPWL.v
+++ siluPWL.v
@@ -25,20 +25,20 @@ end
 assign y = (zero_stage_reg)? 0: ({16{x_stage_reg[15]},x_stage_reg} >> slope_stage_reg)[15:0] + bias_stage_reg;
 /**************** Compare and LUT *****************/
 always @(*) begin
-	if((x - 16'hf000)[15]) begin
+	if((x - 16'hf000)[15]) begin // -8.000000 
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if((x - 16'hfe50)[15]) begin
+	end else 	if((x - 16'hfe50)[15]) begin // -0.843750 
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if((x - 16'hff80)[15]) begin
-		slope = 16'h2;
+	end else 	if((x - 16'hff80)[15]) begin // -0.250000 
+		slope = 16'h2; // 0.250000
 		zero = 0;
 		x_delta = 16'hfe50;
-	end else 	if((x - 16'h108)[15]) begin
-		slope = 16'h1;
+	end else 	if((x - 16'h108)[15]) begin // 0.515625 
+		slope = 16'h1; // 0.500000
 		zero = 0;
 		x_delta = 16'hff80;
 	end else begin
@@ -47,104 +47,104 @@ always @(*) begin
 		x_delta = 16'h108;
 	end
 
-	if((x - 16'hf310)[15]) begin
-		bias = 16'h0;
-	end else 	if((x - 16'hf558)[15]) begin
-		bias = 16'hfff8;
-	end else 	if((x - 16'hf690)[15]) begin
-		bias = 16'hfff0;
-	end else 	if((x - 16'hf760)[15]) begin
-		bias = 16'hffe8;
-	end else 	if((x - 16'hf808)[15]) begin
-		bias = 16'hffe0;
-	end else 	if((x - 16'hf898)[15]) begin
-		bias = 16'hffd8;
-	end else 	if((x - 16'hf918)[15]) begin
-		bias = 16'hffcf;
-	end else 	if((x - 16'hf988)[15]) begin
-		bias = 16'hffc7;
-	end else 	if((x - 16'hf9f0)[15]) begin
-		bias = 16'hffbe;
-	end else 	if((x - 16'hfa58)[15]) begin
-		bias = 16'hffb5;
-	end else 	if((x - 16'hfab0)[15]) begin
-		bias = 16'hffac;
-	end else 	if((x - 16'hfb08)[15]) begin
-		bias = 16'hffa4;
-	end else 	if((x - 16'hfb60)[15]) begin
-		bias = 16'hff9b;
-	end else 	if((x - 16'hfbb8)[15]) begin
-		bias = 16'hff92;
-	end else 	if((x - 16'hfc10)[15]) begin
-		bias = 16'hff8a;
-	end else 	if((x - 16'hfc80)[15]) begin
-		bias = 16'hff81;
-	end else 	if((x - 16'hfd20)[15]) begin
-		bias = 16'hff78;
-	end else 	if((x - 16'hfdf0)[15]) begin
-		bias = 16'hff70;
-	end else 	if((x - 16'hfed0)[15]) begin
-		bias = 16'hff79;
-	end else 	if((x - 16'hff48)[15]) begin
-		bias = 16'hff71;
-	end else 	if((x - 16'hff80)[15]) begin
-		bias = 16'hff7a;
-	end else 	if((x - 16'hff90)[15]) begin
-		bias = 16'hffcc;
-	end else 	if((x - 16'h88)[15]) begin
-		bias = 16'hffc3;
-	end else 	if((x - 16'hc8)[15]) begin
-		bias = 16'hffcd;
-	end else 	if((x - 16'hf8)[15]) begin
-		bias = 16'hffd7;
-	end else 	if((x - 16'h108)[15]) begin
-		bias = 16'hffe2;
-	end else 	if((x - 16'h118)[15]) begin
-		bias = 16'ha8;
-	end else 	if((x - 16'h148)[15]) begin
-		bias = 16'h9d;
-	end else 	if((x - 16'h180)[15]) begin
-		bias = 16'h93;
-	end else 	if((x - 16'h1c8)[15]) begin
-		bias = 16'h89;
-	end else 	if((x - 16'h268)[15]) begin
-		bias = 16'h7f;
-	end else 	if((x - 16'h2f0)[15]) begin
-		bias = 16'h76;
-	end else 	if((x - 16'h380)[15]) begin
-		bias = 16'h7e;
-	end else 	if((x - 16'h3e8)[15]) begin
-		bias = 16'h86;
-	end else 	if((x - 16'h448)[15]) begin
-		bias = 16'h8f;
-	end else 	if((x - 16'h4a0)[15]) begin
-		bias = 16'h98;
-	end else 	if((x - 16'h4f0)[15]) begin
-		bias = 16'ha0;
-	end else 	if((x - 16'h540)[15]) begin
-		bias = 16'ha8;
-	end else 	if((x - 16'h598)[15]) begin
-		bias = 16'hb0;
-	end else 	if((x - 16'h5f8)[15]) begin
-		bias = 16'hb9;
-	end else 	if((x - 16'h660)[15]) begin
-		bias = 16'hc2;
-	end else 	if((x - 16'h6c8)[15]) begin
-		bias = 16'hca;
-	end else 	if((x - 16'h748)[15]) begin
-		bias = 16'hd3;
-	end else 	if((x - 16'h7d0)[15]) begin
-		bias = 16'hdb;
-	end else 	if((x - 16'h880)[15]) begin
-		bias = 16'he4;
-	end else 	if((x - 16'h948)[15]) begin
-		bias = 16'hec;
-	end else 	if((x - 16'ha60)[15]) begin
-		bias = 16'hf4;
-	end else 	if((x - 16'hc40)[15]) begin
-		bias = 16'hfc;
+	if((x - 16'hf310)[15]) begin // -6.468750
+		bias = 16'h0; // 0.000000 
+	end else 	if((x - 16'hf558)[15]) begin // -5.328125
+		bias = 16'hfff8; // -0.015625 
+	end else 	if((x - 16'hf690)[15]) begin // -4.718750
+		bias = 16'hfff0; // -0.031250 
+	end else 	if((x - 16'hf760)[15]) begin // -4.312500
+		bias = 16'hffe8; // -0.046875 
+	end else 	if((x - 16'hf808)[15]) begin // -3.984375
+		bias = 16'hffe0; // -0.062500 
+	end else 	if((x - 16'hf898)[15]) begin // -3.703125
+		bias = 16'hffd8; // -0.078125 
+	end else 	if((x - 16'hf918)[15]) begin // -3.453125
+		bias = 16'hffcf; // -0.095703 
+	end else 	if((x - 16'hf988)[15]) begin // -3.234375
+		bias = 16'hffc7; // -0.111328 
+	end else 	if((x - 16'hf9f0)[15]) begin // -3.031250
+		bias = 16'hffbe; // -0.128906 
+	end else 	if((x - 16'hfa58)[15]) begin // -2.828125
+		bias = 16'hffb5; // -0.146484 
+	end else 	if((x - 16'hfab0)[15]) begin // -2.656250
+		bias = 16'hffac; // -0.164062 
+	end else 	if((x - 16'hfb08)[15]) begin // -2.484375
+		bias = 16'hffa4; // -0.179688 
+	end else 	if((x - 16'hfb60)[15]) begin // -2.312500
+		bias = 16'hff9b; // -0.197266 
+	end else 	if((x - 16'hfbb8)[15]) begin // -2.140625
+		bias = 16'hff92; // -0.214844 
+	end else 	if((x - 16'hfc10)[15]) begin // -1.968750
+		bias = 16'hff8a; // -0.230469 
+	end else 	if((x - 16'hfc80)[15]) begin // -1.750000
+		bias = 16'hff81; // -0.248047 
+	end else 	if((x - 16'hfd20)[15]) begin // -1.437500
+		bias = 16'hff78; // -0.265625 
+	end else 	if((x - 16'hfdf0)[15]) begin // -1.031250
+		bias = 16'hff70; // -0.281250 
+	end else 	if((x - 16'hfed0)[15]) begin // -0.593750
+		bias = 16'hff79; // -0.263672 
+	end else 	if((x - 16'hff48)[15]) begin // -0.359375
+		bias = 16'hff71; // -0.279297 
+	end else 	if((x - 16'hff80)[15]) begin // -0.250000
+		bias = 16'hff7a; // -0.261719 
+	end else 	if((x - 16'hff90)[15]) begin // -0.218750
+		bias = 16'hffcc; // -0.101562 
+	end else 	if((x - 16'h88)[15]) begin // 0.265625
+		bias = 16'hffc3; // -0.119141 
+	end else 	if((x - 16'hc8)[15]) begin // 0.390625
+		bias = 16'hffcd; // -0.099609 
+	end else 	if((x - 16'hf8)[15]) begin // 0.484375
+		bias = 16'hffd7; // -0.080078 
+	end else 	if((x - 16'h108)[15]) begin // 0.515625
+		bias = 16'hffe2; // -0.058594 
+	end else 	if((x - 16'h118)[15]) begin // 0.546875
+		bias = 16'ha8; // 0.328125 
+	end else 	if((x - 16'h148)[15]) begin // 0.640625
+		bias = 16'h9d; // 0.306641 
+	end else 	if((x - 16'h180)[15]) begin // 0.750000
+		bias = 16'h93; // 0.287109 
+	end else 	if((x - 16'h1c8)[15]) begin // 0.890625
+		bias = 16'h89; // 0.267578 
+	end else 	if((x - 16'h268)[15]) begin // 1.203125
+		bias = 16'h7f; // 0.248047 
+	end else 	if((x - 16'h2f0)[15]) begin // 1.468750
+		bias = 16'h76; // 0.230469 
+	end else 	if((x - 16'h380)[15]) begin // 1.750000
+		bias = 16'h7e; // 0.246094 
+	end else 	if((x - 16'h3e8)[15]) begin // 1.953125
+		bias = 16'h86; // 0.261719 
+	end else 	if((x - 16'h448)[15]) begin // 2.140625
+		bias = 16'h8f; // 0.279297 
+	end else 	if((x - 16'h4a0)[15]) begin // 2.312500
+		bias = 16'h98; // 0.296875 
+	end else 	if((x - 16'h4f0)[15]) begin // 2.468750
+		bias = 16'ha0; // 0.312500 
+	end else 	if((x - 16'h540)[15]) begin // 2.625000
+		bias = 16'ha8; // 0.328125 
+	end else 	if((x - 16'h598)[15]) begin // 2.796875
+		bias = 16'hb0; // 0.343750 
+	end else 	if((x - 16'h5f8)[15]) begin // 2.984375
+		bias = 16'hb9; // 0.361328 
+	end else 	if((x - 16'h660)[15]) begin // 3.187500
+		bias = 16'hc2; // 0.378906 
+	end else 	if((x - 16'h6c8)[15]) begin // 3.390625
+		bias = 16'hca; // 0.394531 
+	end else 	if((x - 16'h748)[15]) begin // 3.640625
+		bias = 16'hd3; // 0.412109 
+	end else 	if((x - 16'h7d0)[15]) begin // 3.906250
+		bias = 16'hdb; // 0.427734 
+	end else 	if((x - 16'h880)[15]) begin // 4.250000
+		bias = 16'he4; // 0.445312 
+	end else 	if((x - 16'h948)[15]) begin // 4.640625
+		bias = 16'hec; // 0.460938 
+	end else 	if((x - 16'ha60)[15]) begin // 5.187500
+		bias = 16'hf4; // 0.476562 
+	end else 	if((x - 16'hc40)[15]) begin // 6.125000
+		bias = 16'hfc; // 0.492188 
 	end else begin
-		bias = 16'h104;
+		bias = 16'h104; // 0.492188 
 	end
 end
 
--- tanhPWL.v
+++ tanhPWL.v
@@ -25,48 +25,48 @@ end
 assign y = (zero_stage_reg)? 0: ({16{x_stage_reg[15]},x_stage_reg} >> slope_stage_reg)[15:0] + bias_stage_reg;
 /**************** Compare and LUT *****************/
 always @(*) begin
-	if((x - 16'hf000)[15]) begin
+	if((x - 16'hf000)[15]) begin // -8.000000 
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if((x - 16'hfb28)[15]) begin
+	end else 	if((x - 16'hfb28)[15]) begin // -2.421875 
 		slope = 16'h0;
 		zero = 1;
 		x_delta = 16'hf000;
-	end else 	if((x - 16'hfc48)[15]) begin
-		slope = 16'h4;
+	end else 	if((x - 16'hfc48)[15]) begin // -1.859375 
+		slope = 16'h4; // 0.062500
 		zero = 0;
 		x_delta = 16'hfb28;
-	end else 	if((x - 16'hfd08)[15]) begin
-		slope = 16'h3;
+	end else 	if((x - 16'hfd08)[15]) begin // -1.484375 
+		slope = 16'h3; // 0.125000
 		zero = 0;
 		x_delta = 16'hfc48;
-	end else 	if((x - 16'hfdd8)[15]) begin
-		slope = 16'h2;
+	end else 	if((x - 16'hfdd8)[15]) begin // -1.078125 
+		slope = 16'h2; // 0.250000
 		zero = 0;
 		x_delta = 16'hfd08;
-	end else 	if((x - 16'hfee8)[15]) begin
-		slope = 16'h1;
+	end else 	if((x - 16'hfee8)[15]) begin // -0.546875 
+		slope = 16'h1; // 0.500000
 		zero = 0;
 		x_delta = 16'hfdd8;
-	end else 	if((x - 16'h118)[15]) begin
-		slope = 16'h0;
+	end else 	if((x - 16'h118)[15]) begin // 0.546875 
+		slope = 16'h0; // 1.000000
 		zero = 0;
 		x_delta = 16'hfee8;
-	end else 	if((x - 16'h228)[15]) begin
-		slope = 16'h1;
+	end else 	if((x - 16'h228)[15]) begin // 1.078125 
+		slope = 16'h1; // 0.500000
 		zero = 0;
 		x_delta = 16'h118;
-	end else 	if((x - 16'h2f8)[15]) begin
-		slope = 16'h2;
+	end else 	if((x - 16'h2f8)[15]) begin // 1.484375 
+		slope = 16'h2; // 0.250000
 		zero = 0;
 		x_delta = 16'h228;
-	end else 	if((x - 16'h3b8)[15]) begin
-		slope = 16'h3;
+	end else 	if((x - 16'h3b8)[15]) begin // 1.859375 
+		slope = 16'h3; // 0.125000
 		zero = 0;
 		x_delta = 16'h2f8;
-	end else 	if((x - 16'h4d8)[15]) begin
-		slope = 16'h4;
+	end else 	if((x - 16'h4d8)[15]) begin // 2.421875 
+		slope = 16'h4; // 0.062500
 		zero = 0;
 		x_delta = 16'h3b8;
 	end else begin
@@ -75,60 +75,60 @@ always @(*) begin
 		x_delta = 16'h4d8;
 	end
 
-	if((x - 16'hf000)[15]) begin
-		bias = 16'h0;
-	end else 	if((x - 16'hf9d8)[15]) begin
-		bias = 16'hfdfd;
-	end else 	if((x - 16'hfc48)[15]) begin
-		bias = 16'hfe06;
-	end else 	if((x - 16'hfc98)[15]) begin
-		bias = 16'hfe1c;
-	end else 	if((x - 16'hfcf8)[15]) begin
-		bias = 16'hfe14;
-	end else 	if((x - 16'hfd08)[15]) begin
-		bias = 16'hfe1d;
-	end else 	if((x - 16'hfd20)[15]) begin
-		bias = 16'hfe36;
-	end else 	if((x - 16'hfdc0)[15]) begin
-		bias = 16'hfe2e;
-	end else 	if((x - 16'hfdd8)[15]) begin
-		bias = 16'hfe38;
-	end else 	if((x - 16'hfde8)[15]) begin
-		bias = 16'hfe6e;
-	end else 	if((x - 16'hfea0)[15]) begin
-		bias = 16'hfe65;
-	end else 	if((x - 16'hfed8)[15]) begin
-		bias = 16'hfe6f;
-	end else 	if((x - 16'hfee8)[15]) begin
-		bias = 16'hfe79;
-	end else 	if((x - 16'hfef0)[15]) begin
-		bias = 16'hff05;
-	end else 	if((x - 16'hff18)[15]) begin
-		bias = 16'hfefc;
-	end else 	if((x - 16'hff50)[15]) begin
-		bias = 16'hfef4;
-	end else 	if((x - 16'h68)[15]) begin
-		bias = 16'hfeec;
-	end else 	if((x - 16'hc8)[15]) begin
-		bias = 16'hfee4;
-	end else 	if((x - 16'h100)[15]) begin
-		bias = 16'hfedb;
-	end else 	if((x - 16'h118)[15]) begin
-		bias = 16'hfed2;
-	end else 	if((x - 16'h140)[15]) begin
-		bias = 16'h102;
-	end else 	if((x - 16'h178)[15]) begin
-		bias = 16'h10b;
-	end else 	if((x - 16'h228)[15]) begin
-		bias = 16'h113;
-	end else 	if((x - 16'h2f8)[15]) begin
-		bias = 16'h199;
-	end else 	if((x - 16'h3b8)[15]) begin
-		bias = 16'h1d1;
-	end else 	if((x - 16'h4d8)[15]) begin
-		bias = 16'h1eb;
+	if((x - 16'hf000)[15]) begin // -8.000000
+		bias = 16'h0; // 0.000000 
+	end else 	if((x - 16'hf9d8)[15]) begin // -3.078125
+		bias = 16'hfdfd; // -1.005859 
+	end else 	if((x - 16'hfc48)[15]) begin // -1.859375
+		bias = 16'hfe06; // -0.988281 
+	end else 	if((x - 16'hfc98)[15]) begin // -1.703125
+		bias = 16'hfe1c; // -0.945312 
+	end else 	if((x - 16'hfcf8)[15]) begin // -1.515625
+		bias = 16'hfe14; // -0.960938 
+	end else 	if((x - 16'hfd08)[15]) begin // -1.484375
+		bias = 16'hfe1d; // -0.943359 
+	end else 	if((x - 16'hfd20)[15]) begin // -1.437500
+		bias = 16'hfe36; // -0.894531 
+	end else 	if((x - 16'hfdc0)[15]) begin // -1.125000
+		bias = 16'hfe2e; // -0.910156 
+	end else 	if((x - 16'hfdd8)[15]) begin // -1.078125
+		bias = 16'hfe38; // -0.890625 
+	end else 	if((x - 16'hfde8)[15]) begin // -1.046875
+		bias = 16'hfe6e; // -0.785156 
+	end else 	if((x - 16'hfea0)[15]) begin // -0.687500
+		bias = 16'hfe65; // -0.802734 
+	end else 	if((x - 16'hfed8)[15]) begin // -0.578125
+		bias = 16'hfe6f; // -0.783203 
+	end else 	if((x - 16'hfee8)[15]) begin // -0.546875
+		bias = 16'hfe79; // -0.763672 
+	end else 	if((x - 16'hfef0)[15]) begin // -0.531250
+		bias = 16'hff05; // -0.490234 
+	end else 	if((x - 16'hff18)[15]) begin // -0.453125
+		bias = 16'hfefc; // -0.507812 
+	end else 	if((x - 16'hff50)[15]) begin // -0.343750
+		bias = 16'hfef4; // -0.523438 
+	end else 	if((x - 16'h68)[15]) begin // 0.203125
+		bias = 16'hfeec; // -0.539062 
+	end else 	if((x - 16'hc8)[15]) begin // 0.390625
+		bias = 16'hfee4; // -0.554688 
+	end else 	if((x - 16'h100)[15]) begin // 0.500000
+		bias = 16'hfedb; // -0.572266 
+	end else 	if((x - 16'h118)[15]) begin // 0.546875
+		bias = 16'hfed2; // -0.589844 
+	end else 	if((x - 16'h140)[15]) begin // 0.625000
+		bias = 16'h102; // 0.503906 
+	end else 	if((x - 16'h178)[15]) begin // 0.734375
+		bias = 16'h10b; // 0.521484 
+	end else 	if((x - 16'h228)[15]) begin // 1.078125
+		bias = 16'h113; // 0.537109 
+	end else 	if((x - 16'h2f8)[15]) begin // 1.484375
+		bias = 16'h199; // 0.798828 
+	end else 	if((x - 16'h3b8)[15]) begin // 1.859375
+		bias = 16'h1d1; // 0.908203 
+	end else 	if((x - 16'h4d8)[15]) begin // 2.421875
+		bias = 16'h1eb; // 0.958984 
 	end else begin
-		bias = 16'h1fb;
+		bias = 16'h1fb; // 0.958984 
 	end
 end
 
